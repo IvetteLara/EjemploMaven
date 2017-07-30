b325a6c714f786ccf495e3691f0cb1fb2c972288
IvetteLara <ivette.lara2012@gmail.com>
2017-07-30 13:31:42 -0600

    Commit Call Reporte Vuelos

---
+++ src/main/java/com/mycompany/reportes/controlReporteVuelos.java
@@ -0,0 +1,58 @@
+package com.mycompany.reportes;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+import net.sf.jasperreports.engine.JRException;
+
+
+@ManagedBean
+@ViewScoped
+public class controlReporteVuelos implements Serializable {
+
+    private Date fecha1;
+    private Date fecha2;
+    private String tipo;
+    
+    public controlReporteVuelos() {
+    }
+
+    public void generarReporte() throws JRException, IOException {
+        Reportes r = new Reportes();
+        Map parametros = new HashMap();
+        parametros.put("fechaInicial", fecha1);
+        parametros.put("fechaFinal", fecha2);
+        parametros.put("estado", "v.estado in ('DISPONIBLE','NO DISPONIBLE')");
+        r.generarReporte(parametros, tipo, "./reportes/Reporte_Vuelos.jasper", "Reporte_Vuelos");
+    }
+    
+    public Date getFecha1() {
+        return fecha1;
+    }
+
+    public void setFecha1(Date fecha1) {
+        this.fecha1 = fecha1;
+    }
+
+    public Date getFecha2() {
+        return fecha2;
+    }
+
+    public void setFecha2(Date fecha2) {
+        this.fecha2 = fecha2;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+}
